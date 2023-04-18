6e90c68b320d5f765208b3a44775b77a68c0d20c
Vlad Gohn <vladgohn@gmail.com>
2023-04-18 17:38:13 +0300

    resolve merge conflict3

--- run.sh
+++ run.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
-echo "Создание и запуск скрипта..."
+echo " ( \   .-'''-.  A.-.A    "
+echo "  \.\/        \/ , , \   "
+echo "   \.  . dFo  =; >t< /=  "
+echo "    \   \ ...  . ,¯¯     "
+echo "    / / /    | | |       "
+echo "    ⦦,),)     ⦦,),)     "
 echo '#!/bin/bash' > run_temp.sh
 echo 'chmod +x change_vscode_icon.sh' >> run_temp.sh
 echo './change_vscode_icon.sh' >> run_temp.sh
 chmod +x run_temp.sh
 sudo ./run_temp.sh
 rm run_temp.sh
-echo "Скрипт завершен!"
+echo "End!"
