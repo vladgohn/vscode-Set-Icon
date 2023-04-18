7845627247627cd90402cdaa4eddde3342d5522f
Vlad Gohn <vladgohn@gmail.com>
2023-04-18 17:36:40 +0300

    resolve merge conflict3

--- run.sh
+++ run.sh
@@ -1,12 +1,4 @@
 #!/bin/bash
-<<<<<<< HEAD
-
-# Даем права на выполнение скрипта change_vscode_icon.sh
-chmod +x change_vscode_icon.sh
-
-# Запускаем скрипт change_vscode_icon.sh
-./change_vscode_icon.sh
-=======
 echo "Создание и запуск скрипта..."
 echo '#!/bin/bash' > run_temp.sh
 echo 'chmod +x change_vscode_icon.sh' >> run_temp.sh
@@ -15,4 +7,3 @@ chmod +x run_temp.sh
 sudo ./run_temp.sh
 rm run_temp.sh
 echo "Скрипт завершен!"
->>>>>>> eacd71d (update2)
